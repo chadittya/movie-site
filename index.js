8199568b2f95e9e4b79c18c8de495603d713f705
Galih <test@localhost>
2024-07-07 00:36:23 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f3723640fd9f809825ccbfe6beb3101f&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=f3723640fd9f809825ccbfe6beb3101f&query=";
+
